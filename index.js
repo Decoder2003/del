1345d97e90324e81a6b27af3efb54d11551cfa61
Decoder2003 <dhruvkalra1307@gmail.com>
2025-02-15 14:24:25 +0530

    added swagegr

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
 const express = require("express");
 const app = express();
 
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 // middleware
 app.use(express.json());
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Serve static files
+app.use(express.static("public"));
 
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Express API with Swagger",
+            version: "1.0.0",
+            description: "A simple API documentation using Swagger",
+        },
+        servers: [
+            {
+                url: "http://localhost:8000",
+                description: "Local server",
+            },
+            { url: "https://del-nu.vercel.app" }
+        ],
+    },
+    apis: ["./index.js"], // Points to this file for Swagger annotations
+};
 
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Welcome route
+ *     responses:
+ *       200:
+ *         description: Returns a welcome message
+ */
 app.get("/", (req, res) => {
     res.send("Hey! there.");
 })
 
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Fetches sample posts
+ *     responses:
+ *       200:
+ *         description: Returns an array of posts
+ */
 app.get("/posts", async (req, res) => {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await response.json();
