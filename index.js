86e28cab920f2eb3805fbe12e75841182bed4ea8
Decoder2003 <dhruvkalra1307@gmail.com>
2025-02-15 14:13:12 +0530

    do it

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// middleware
+app.use(express.json());
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.get("/posts", async (req, res) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    res.send(data);
+})
+
+// Start server
+app.listen(8000, () => { console.log("Server running at PORT:8000") });
