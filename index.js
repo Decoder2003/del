6c7dbd975769c54de2918aed0820eaae0b2708d5
Decoder2003 <dhruvkalra1307@gmail.com>
2025-02-15 14:17:19 +0530

    adding beauty

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json());
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
+app.get("/", (req, res) => {
+    res.send("Hey! there.");
+})
+
 app.get("/posts", async (req, res) => {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await response.json();
